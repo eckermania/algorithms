64155485e5bf45c1f98f227ee295dbf3d149ca1b
Erin Eckerman <erineckerman@gmail.com>
2019-05-02 16:22:45 -0700

    completed string pattern challenge

---
+++ string-pattern/string-pattern.js
@@ -0,0 +1,26 @@
+let wordPattern = function(pattern, string){
+    let pattArr = pattern.split('');
+    let strArr = string.split(' ');
+    let container = [];
+    let pattIndices = [];
+    let strIndices = [];
+  
+    pattArr.map(el => {
+      if(container.includes(el)){
+        pattIndices.push(pattArr.indexOf(el))
+      } else {
+        container.push(el);
+        pattIndices.push(pattArr.indexOf(el))
+      }
+    })
+    container = [];
+    strArr.map(el => {
+      if(container.includes(el)){
+        strIndices.push(strArr.indexOf(el))
+      } else {
+        container.push(el);
+        strIndices.push(strArr.indexOf(el))
+      }
+    })
+    return pattIndices.join('') === strIndices.join('');
+  }
