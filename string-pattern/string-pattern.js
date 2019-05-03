d9acbe3bb30c6e7754c54c3052637b5ae4c6f977
Erin Eckerman <erineckerman@gmail.com>
2019-05-03 09:16:28 -0700

    refactor string pattern

--- string-pattern/string-pattern.js
+++ string-pattern/string-pattern.js
@@ -4,26 +4,13 @@ let wordPattern = function(pattern, string){
     if (pattArr.length !== strArr.length){
       return false
     } else {
-    let container = [];
     let pattIndices = [];
     let strIndices = [];
-  
     pattArr.map(el => {
-      if(container.includes(el)){
         pattIndices.push(pattArr.indexOf(el))
-      } else {
-        container.push(el);
-        pattIndices.push(pattArr.indexOf(el))
-      }
     })
-    container = [];
     strArr.map(el => {
-      if(container.includes(el)){
         strIndices.push(strArr.indexOf(el))
-      } else {
-        container.push(el);
-        strIndices.push(strArr.indexOf(el))
-      }
     })
     return pattIndices.join('') === strIndices.join('');
   }
